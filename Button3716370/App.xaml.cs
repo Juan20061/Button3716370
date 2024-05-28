b134ee9391a01d128f6086e2cc881d3bff2d89c4
Juan Carlos <Juan Carlos@DESKTOP-QQANMR9>
2024-05-28 14:56:25 -0600

    Agregar archivos de proyecto.

---
+++ Button3716370/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Button3716370
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Button3716370/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Button3716370
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
